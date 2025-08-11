9dab1ff296d8e562495a55ff35e0b1a3e515d7ad
Weso <mohamed.hosam.eldeeen@gmail.com>
2025-08-11 18:46:20 +0300

    Update budgetapp.py

--- budgetapp.py
+++ budgetapp.py
@@ -2,6 +2,32 @@ class Category:
     def __init__(self, name):
         self.name = name 
         self.ledger = []
-
+    
+    def deposit(self, amount, description=""):
+        self.ledger.append({"amount": amount, "description": description})
+    
+    def withdraw(self, amount, description = ""):
+        if self.check_funds(amount):
+            self.ledger.append({"amount": -amount, "description": description})
+            return True
+        return False
+    
+    def get_balance(self):
+        total = 0
+        for item in self.ledger:
+            total += item["amount"]
+        return total
+    
+    
+    def transfer(self, amount, category):
+        if self.check_funds(amount):
+            self.withdraw(amount, f"Transfter to {category.name}")
+            category.deposit(amount, f"Transfer from {self.name}")
+            return True
+        return False
+    
+    def check_funds(self, amount):
+        return amount <= self.get_balance
+    
 def create_spend_chart(categories):
     pass
